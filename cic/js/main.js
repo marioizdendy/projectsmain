ddc229f7902083a75a62d7f06a6fc69c8218c356
marioizdendy <89105851+marioizdendy@users.noreply.github.com>
2021-09-01 14:23:17 +0300

    Add files via upload

---
+++ cic/js/main.js
@@ -0,0 +1,14 @@
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+        change.target.classList.add('element-show');
+      }
+    });
+  }
+  let options = { threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.el-anim');
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
+
